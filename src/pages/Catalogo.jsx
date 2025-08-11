5bdb130b0619514b81493aa379b5c2ce9c8171c3
Miguel Mateos <miguelmateosruiz@gmail.com>
2025-08-11 14:48:15 +0200

    crear formulario de contacto

--- src/pages/Catalogo.jsx
+++ src/pages/Catalogo.jsx
@@ -1,9 +1,43 @@
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 export function Catalogo() {
+  
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_rc2e8n8', 'template_ov96b6d', form.current, {
+        publicKey: '0I1EqHAzq6oiHJHkb',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
+  //Pendiente validar que el email tenga un formato correcto
+  //Pendiente validar que los campos no estén vacíos
   return (
     <main>
       <h1>Catálogo</h1>
       <p>Descubre los productos que ofrecemos para llevar tus ideas al siguiente nivel.</p>
-      {/* Aquí puedes añadir más contenido o componentes */}
+      <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="name" />
+
+      <label>Email</label>
+      <input type="email" name="email" />
+
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
     </main>
   );
 }
