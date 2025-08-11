427a55d4c6744c5306ee6f4cafe5903e95cf0162
Miguel Mateos <miguelmateosruiz@gmail.com>
2025-08-11 10:51:39 +0200

    crear compnentes principales

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <header>
+      <h1>Nymphe Spirit Studio</h1>
+      
+    </header>
+  );
+}
---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+export function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Inicio</a></li>
+        <li><a href="/about">Sobre Nosotros</a></li>
+        <li><a href="/portfolio">Portafolio</a></li>
+        <li><a href="/shop">Tienda</a></li>
+        <li><a href="/services">Servicios</a></li>
+        <li><a href="/cart">Carrito</a></li>
+      </ul>
+    </nav>
+  );
+}
