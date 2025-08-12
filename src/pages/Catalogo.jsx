48f35eb89fad401fba7d3ab24306ffa9e7d9d19c
Miguel Mateos <miguelmateosruiz@gmail.com>
2025-08-12 09:58:38 +0200

    Poner variables de entorno y añadir tipografias

--- src/pages/Catalogo.jsx
+++ src/pages/Catalogo.jsx
@@ -1,5 +1,7 @@
 import { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+
+
 export function Catalogo() {
   
   const form = useRef();
@@ -7,9 +9,11 @@ export function Catalogo() {
   const sendEmail = (e) => {
     e.preventDefault();
 
+    if (!form.current) return;
+
     emailjs
-      .sendForm('service_rc2e8n8', 'template_ov96b6d', form.current, {
-        publicKey: '0I1EqHAzq6oiHJHkb',
+      .sendForm(import.meta.env.VITE_EMAIL_SERVICE, import.meta.env.VITE_EMAIL_TEMPLATE, form.current, {
+        publicKey: import.meta.env.VITE_EMAIL_PUBLIC_KEY,
       })
       .then(
         () => {
@@ -23,21 +27,22 @@ export function Catalogo() {
 
   //Pendiente validar que el email tenga un formato correcto
   //Pendiente validar que los campos no estén vacíos
+  //Borrar los campos del formulario después de enviar
   return (
     <main>
       <h1>Catálogo</h1>
       <p>Descubre los productos que ofrecemos para llevar tus ideas al siguiente nivel.</p>
       <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="name" />
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" />
 
-      <label>Email</label>
-      <input type="email" name="email" />
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" />
 
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+        <label htmlFor="message">Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
     </main>
   );
 }
