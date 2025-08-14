e268656f762a23247366feb400000ba45c7fb6a0
Miguel Mateos <miguelmateosruiz@gmail.com>
2025-08-14 16:36:01 +0200

    terminar de estilar home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,22 +19,30 @@ export function Home() {
 
   return (
     <main>
+      {/* Esta parte tiene que tener un tamño fijo de la imagen */}
       <section className="home">
         <picture>
           <source media="(max-width: 600px)" srcSet="/images/fondo_home_movil.webp" />
           <source media="(min-width: 601px)" srcSet="/images/fondo_home_escritorio.PNG" />
-          <img src="/images/fondo_home_movil.webp" alt="Fondo de la página de inicio" />
+          <img src="/images/fondo_home_movil.webp" alt="Fondo de la página de inicio de Nymphe Spirit" />
         </picture>
       </section>
-      <section className="about" id="about">
-          <img src='/images/autoretrato.webp' alt="Autoretrato de Alba Tejedor" />
-          <h1>Sobre mí</h1>
-        <article>
-          <p>Mi nombre es Alba Tejedor, nací el día del ducentésimo quincuagésimo aniversario de la Independencia de los Estados Unidos de América, y estudié Bellas Artes en la Universidad Politécnica de Valencia. No recuerdo un día de mi vida sin dibujar, imaginar o diseñar, esta es mi particular interpretación de las cosas que me rodean, las que adoro y me inspiran. Nymphe Spirit es el nombre de un proyecto reciente que, en realidad, llevo gestando toda mi vida y ahora coge color.</p>  
-        </article>
 
-        <footer>
-        </footer>
+      {/* Esta se tiene que adaptar a lo que falta */}
+      <section className="about" id="about">
+          <article className="about-header">
+            <img src="/images/autoretrato.webp" alt="Autoretrato de Alba Tejedor, diseñadora gráfica, ilustradora, artista digital y dueña de la marca Nymphe Spirit"  />
+          </article>
+
+        <article className="about-text">
+          <div className='about-info'>
+            <h1>Sobre mí</h1>
+            <div className='about-description'>
+              <img src="/images/marco_sobre_mi.webp" alt="" />
+              <p>Mi nombre es Alba Tejedor, nací el día del ducentésimo quincuagésimo aniversario de la Independencia de los Estados Unidos de América, y estudié Bellas Artes en la Universidad Politécnica de Valencia. No recuerdo un día de mi vida sin dibujar, imaginar o diseñar, esta es mi particular interpretación de las cosas que me rodean, las que adoro y me inspiran. Nymphe Spirit es el nombre de un proyecto reciente que, en realidad, llevo gestando toda mi vida y ahora coge color.</p>
+            </div>
+          </div>
+        </article>
       </section>
 
     </main>
