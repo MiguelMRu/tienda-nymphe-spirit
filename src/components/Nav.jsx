7597eb67aaf5165efb665f7041a4efb40cd4cf8b
Miguel Mateos <miguelmateosruiz@gmail.com>
2025-08-15 18:21:10 +0200

    cambios menoeres de correccciones, añadir que se cierre el menu al clicar en el nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -20,7 +20,7 @@ export function Nav({ isOpen, toggleMenu }) {
   };
 
   return (
-    <nav className={navClass}>
+    <nav className={navClass} onClick={toggleMenu}>
       <button className="close-button" onClick={toggleMenu}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
